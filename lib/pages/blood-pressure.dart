93ccef5505c5a177c69e8b7dd87fd7b3eb00a4f8
fredrick rogath <fredlee560@gmail.com>
2022-04-20 09:42:35 +0300

    dmts

--- lib/pages/blood-pressure.dart
+++ lib/pages/blood-pressure.dart
@@ -17,7 +17,7 @@ class _BloodPressureState extends State<BloodPressure> {
           Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: const[Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: EdgeInsets.all(8.0),
                 child: Text('Total records 0'),
               )]),
           Lottie.asset('assets/boodpressure.json')
