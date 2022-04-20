bbc5ccd83d59f28a3162f726e47e5c1a4391c00a
fredrick rogath <fredlee560@gmail.com>
2022-04-20 08:27:00 +0300

    dmts

--- lib/pages/blood-glucose.dart
+++ lib/pages/blood-glucose.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+
 class BloodGlucose extends StatefulWidget {
   BloodGlucose({Key? key}) : super(key: key);
 
@@ -11,14 +12,17 @@ class _BloodGlucoseState extends State<BloodGlucose> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: const Color(0xFF004B23)),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF004B23),
+          actions:[IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        ),
         body: Column(children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const[Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Total records 0'),
-              )]),
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Total records 0'),
+            )
+          ]),
           Lottie.asset('assets/glucometer.json')
         ]));
   }
--- lib/pages/blood-pressure.dart
+++ lib/pages/blood-pressure.dart
@@ -12,11 +12,11 @@ class _BloodPressureState extends State<BloodPressure> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: const Color(0xFF004B23)),
+        appBar: AppBar(backgroundColor: const Color(0xFF004B23),actions:[IconButton(onPressed: () {}, icon: const Icon(Icons.add))],),
         body: Column(children: [
           Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children: [Padding(
+              children: const[Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text('Total records 0'),
               )]),
